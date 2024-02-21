446bf3569182ad7ea290cb27af820a61cc779048
arijit <arijitsarkar2111@gmail.com>
2024-02-21 23:33:55 +0530

    Update New One

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -8,9 +8,9 @@ const Count = () => {
         <>
         <div className="main_contect">
             <div className="btn-group me-2" role="group" aria-label="Second group">
-                <button type="button" className="btn btn-secondary" onClick={() => dispatch(decNumber())}>-</button>
+                <button type="button" className="btn btn-secondary" onClick={() => dispatch(decNumber(2))}>-</button>
                 <button type="button" className="btn btn-dark">  </button>
-                <button type="button" className="btn btn-secondary" onClick={() => dispatch(incNumber())}>+</button>
+                <button type="button" className="btn btn-secondary" onClick={() => dispatch(incNumber(2))}>+</button>
             </div>
         </div>
         </>
