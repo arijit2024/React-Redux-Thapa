e7a3381fe6beea01c4e8af0f07eef10974d93160
arijit <arijitsarkar2111@gmail.com>
2024-02-21 18:43:25 +0530

    Update Three

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import {incNumber, decNumber} from '../actions/index'
 
 const Count = () => {
+    const dispatch = useDispatch();
     return(
         <>
         <div className="main_contect">
             <div className="btn-group me-2" role="group" aria-label="Second group">
-                <button type="button" className="btn btn-secondary">-</button>
+                <button type="button" className="btn btn-secondary" onClick={() => dispatch(decNumber())}>-</button>
                 <button type="button" className="btn btn-dark">  </button>
-                <button type="button" className="btn btn-secondary">+</button>
+                <button type="button" className="btn btn-secondary" onClick={() => dispatch(incNumber())}>+</button>
             </div>
         </div>
         </>
